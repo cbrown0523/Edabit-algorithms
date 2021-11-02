5939d75d62c836de33da6649e8529ae170472732
christine <chrisitne.brown@bhcc.edu>
2021-11-02 03:08:48 -0400

    edabit 11/21

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+
+//get word count
+
+public class Program {
+    public static int countWords(String s){
+        return s.split(" ").length;
+    }
+}
+
+//pair management
+public class Pairs {
+    public static int[] makePair(int a, int b) {
+
+        int[] pair = {a , b};
+
+        return pair;
+
+    }
+}
